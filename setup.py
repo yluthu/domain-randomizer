fab7677ef8b3fc181f79b3d9b04b4545de317a83
Yiwen Lu <ylu.thu15@gmail.com>
2019-12-20 10:56:25 +0800

    install config files on setup

--- setup.py
+++ setup.py
@@ -4,6 +4,8 @@ setup(
     name='domain_randomizer',
     version='0.0.1',
     packages=['randomizer',],
+    package_data={'randomizer': ['config/*/*']},
     license='Creative Commons Attribution-Noncommercial-Share Alike license',
     long_description=open('README.md').read(),
+    include_package_data=True
 )
